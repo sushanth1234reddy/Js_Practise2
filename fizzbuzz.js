df74f9a6e5af686f635826eec616e787ec17292b
Singadwar Sushanth Reddy <76097402+sushanth1234reddy@users.noreply.github.com>
2024-08-19 23:25:43 +0530

    Add files via upload

---
+++ fizzbuzz.js
@@ -0,0 +1,14 @@
+for(let i=1;i<=20;i++){
+    if(i%2==0 && i%3==0){
+        console.log(i+"fizzbuzz");
+    }
+    else if(i%3==0){
+        console.log(i+"buzz");
+    }
+    else if(i%2==0){
+        console.log(i+"fizz");
+    }
+    else{
+        console.log(i+"sushanth");
+    }
+}
