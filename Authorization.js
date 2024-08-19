b79acf43a4cf4a2bb20387e32472888ba2336b55
Singadwar Sushanth Reddy <76097402+sushanth1234reddy@users.noreply.github.com>
2024-08-19 23:24:56 +0530

    Add files via upload

---
+++ Authorization.js
@@ -0,0 +1,16 @@
+let user="customer";
+if(user=="admin"){
+    console.log("Logged in as admin");
+}
+else if(user=="employee"){
+    console.log("Logged in as Employee")
+}
+else if(user == "super_admin"){
+    console.log("Logged in as super admin");
+}
+else if(user=="customer"){
+    console.log("Logged in as customer")
+}
+else{
+    console.log("Unauthorized User")
+}
