ca729776466836fa17f55b7cc60fad1973ffa6fe
Singadwar Sushanth Reddy <76097402+sushanth1234reddy@users.noreply.github.com>
2024-08-19 23:25:29 +0530

    Add files via upload

---
+++ day7.js
@@ -0,0 +1,20 @@
+let marks=67;
+if(marks>=90 && marks<=100){
+   console.log("distinction")
+}
+else if(marks>=75 && marks<=89){
+   console.log("First Class")
+}
+else if(marks>=60 && marks<=74){
+   console.log("sceond class")
+}
+else if(marks>=35 && marks<=59){
+    console.log("Third Class")
+}
+else if(marks>=0 && marks<=35)
+{
+    console.log("Fail")
+}
+else{
+    console.log("invalid marks")
+}
